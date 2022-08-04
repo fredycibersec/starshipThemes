d619080c1bc898410cb32ef246c5522efb540b6e
Fredy Master.D <26630788+docentemasterd@users.noreply.github.com>
2022-08-04 15:58:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-echo "Instalación del fichero de usuario."
-echo "Para instalarlo como root, cambia con 'sudo -s'."
 
-if [ -d ~/.config ]
+user=$(whoami)
+file=user_starship.toml
+
+
+function copiaFile(){
+	echo "Copiando en..."
+	echo "~/.config"
+	cp $file $HOME/.config/starship.toml
+}
+
+function compDir(){
+	if [ -d ~/.config ] 
+		then 
+			copiaFile 
+		else 
+			echo "No existe el directorio destino ~/.config"
+			echo "Deseas crearlo y copiar el fichero en él? (s/n)"
+			read resp
+			if [ $resp == 's']
+			then
+				echo "Creando directorio..."
+				mkdir ~/.config
+				copiaFile
+			else
+				echo "Saliendo."
+				exit 0
+			fi
+		fi
+}
+
+
+echo "Instalación del fichero de configuración."
+echo ""
+if [ "$EUID" -ne 0 ]
 then
-	echo "Copiando en "$HOME"/.config"
-	cp ./user_starship.toml $HOME/.config/starship.toml
+	echo "Para instalarlo en el usuario root, eleva privilegios: p.ej. [sudo -s]"
+	echo "Instalando el fichero de configuración para el usuario: " $user
+	echo ""
+	compDir
 	exit 0
 else
-	echo "No existe el directorio destino ~/.config"
+	file=root_starship.toml
+	compDir
 fi
