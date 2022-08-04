191084854372bce21d861060d89bfb082d53d20f
Fredy Master.D <26630788+docentemasterd@users.noreply.github.com>
2022-08-04 13:46:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Para instalarlo como root, cambia con 'sudo -s'."
 if [ -d ~/.config ]
 then
 	echo "Copiando en "$HOME"/.config"
-	#cp ./user_starship.toml $HOME/.config/starship.toml
+	cp ./user_starship.toml $HOME/.config/starship.toml
 	exit 0
 else
 	echo "No existe el directorio destino ~/.config"
